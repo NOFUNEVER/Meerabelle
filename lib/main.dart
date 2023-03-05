1b3bf6923489779e8e6b16439e5cb2143cb7989c
Jason Lamphere <jason.lamphere@gmail.com>
2023-03-04 18:47:15 -0800

    new branch

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: _title,
       theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
+
       home:  LoginScreen(),
     );
   }
